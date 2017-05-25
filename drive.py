db309177fc3db4e220e6293897ff164c7e0d34ce
Rustem Iskuzhin <rustem22@mail.ru>
2017-05-25 08:15:50 +0500

    Add files via upload

--- drive.py
+++ drive.py
@@ -47,35 +47,39 @@ controller = SimplePIController(0.1, 0.002)
 set_speed = 9
 controller.set_desired(set_speed)
 
+def crop_camera(img):
+	height = img.shape[0]
+	y_start = 50
+	y_stop = height-20
+	return img[y_start:y_stop, :]
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    if data:
-        # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
-        # The current speed of the car
-        speed = data["speed"]
-        # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+	if data:
+		# The current steering angle of the car
+		steering_angle = data["steering_angle"]
+		# The current throttle of the car
+		throttle = data["throttle"]
+		# The current speed of the car
+		speed = data["speed"]
+		# The current image from the center camera of the car
+		imgString = data["image"]
+		image = Image.open(BytesIO(base64.b64decode(imgString)))
+		image_array = np.asarray(image)
+		image_array = crop_camera(image_array)
+		steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+		throttle = controller.update(float(speed))
+		print(steering_angle, throttle)
+		send_control(steering_angle, throttle)
 
-        throttle = controller.update(float(speed))
-
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
-
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
-    else:
-        # NOTE: DON'T EDIT THIS.
-        sio.emit('manual', data={}, skip_sid=True)
+		# save frame
+		if args.image_folder != '':
+			timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+			image_filename = os.path.join(args.image_folder, timestamp)
+			image.save('{}.jpg'.format(image_filename))
+	else:
+		# NOTE: DON'T EDIT THIS.
+		sio.emit('manual', data={}, skip_sid=True)
 
 
 @sio.on('connect')
